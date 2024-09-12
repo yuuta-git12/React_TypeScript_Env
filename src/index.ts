dd4054f5987548966f2f222ddeb4c92d6d154eca
yuuta-git12 <42604764+yuuta-git12@users.noreply.github.com>
2024-09-12 22:56:05 +0900

    切り上げ関数のテスト追加

--- src/index.ts
+++ src/index.ts
@@ -18,3 +18,23 @@ middle_name  = "doragon";
 console.log(middle_name + third_name);
 middle_name = "ryu";
 // third_name = "tora";
+
+// 引数の値を100円単位で切り上げる関数
+const roundupPriceBy100 = (price: number): number =>  price + (100 - price%100)%100;
+
+
+const calcu_test = () => {
+    "use strict";
+
+    console.log(roundupPriceBy100(22));
+    console.log(roundupPriceBy100(1022));
+    console.log(roundupPriceBy100(1001));
+    console.log(roundupPriceBy100(11990));
+    console.log(roundupPriceBy100(129999));
+    console.log(roundupPriceBy100(128990));     
+}
+
+calcu_test();
+
+
+
